a543941e83c18504fd8a4c22034d79522a562e8d
linkvetern64 <jstand1@umbc.edu>
2017-12-09 01:13:48 -0500

    clean up

--- proj2/milestone2/main.cpp
+++ proj2/milestone2/main.cpp
@@ -26,7 +26,7 @@ std::vector<char> read(std::string file, int len);
  *        Main Function       *
  ******************************/
 int main(int argc, char *argv[]) {
-
+  
     //If the arguments don't match the argument signature, print guide.
     if(argc != 5){
         std::cout << "Program does not match testing signature." << std::endl;
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
     //Read files in and parse them
     X = read(file1, n);
     Y = read(file2, m);
-
+    
     //Call LCS
     std::cout << "Serial LCS is " << lcs(X, Y, n, m) << std::endl;
     std::cout << "Parallel LCS is " << parallel_lcs(X, Y, n, m) << std::endl;
@@ -97,28 +97,11 @@ std::vector<char> read(std::string file, int len){
 int parallel_lcs(std::vector<char> X, std::vector<char> Y, int n, int m){
     //Clear L[n + 1][m + 1] with 0's
     int L[n + 1][m + 1];
-    std::vector<char> LCS;
-
-
-    
 
     //Compute LCS matrix
-    
     for(int i = 0; i <= n; i++){
         for(int j = 0; j <= m; j++){
 	  L[i][j] = 0;
-	    /*
-	  if(i == 0 || j == 0){
-                L[i][j] = 0;
-            }
-            else if(X[i - 1] == Y[j - 1]){
-                L[i][j] = L[i - 1][j - 1] + 1;
-            }
-            else{
-                L[i][j] = std::max(L[i - 1][j], L[i][j - 1]);
-            }
-
-	  */
         }
     }
     
@@ -134,7 +117,6 @@ int parallel_lcs(std::vector<char> X, std::vector<char> Y, int n, int m){
       
 	int count = std::min(i, std::min((COL - start_col), ROW));
 
-	//	std::cout << X[i - 1] << std::endl;
 
 	#pragma omp parallel for
 	for (int j = 0; j < count; j++){
